2b22c8568bc482b6cabd3396587566929b1bb410
Waqas <wmunir232@gmail.com>
2023-11-02 14:47:46 +0500

    Union Type in TS

---
+++ ReactTS/Intro-To-Typescript/userID.ts
@@ -0,0 +1,7 @@
+// * Idea behind this program is to get the user ID that can either be string or number
+
+let userID: number | string; // ? defining union types that it can be either string or number
+
+userID = "abd123";
+
+userID = 123;
