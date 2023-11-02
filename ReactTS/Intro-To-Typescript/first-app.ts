a79abaaacb9f196cca639a9d3818f37417744e9d
Waqas <wmunir232@gmail.com>
2023-11-02 14:45:00 +0500

    Basic TS Types

--- ReactTS/Intro-To-Typescript/first-app.ts
+++ ReactTS/Intro-To-Typescript/first-app.ts
@@ -3,3 +3,7 @@ let userName = "Waqas"; // ? Automatic type inference at initialization based on
 let num: number; // ? Explicitly assigning types
 
 // userName = 34; // ! Can't assign number datatype value to string variable
+
+// * Basic Types in TS
+
+// ? string, number, boolean
