8704a6b63be65d8c7769c9107a34c83734f995ff
Waqas <wmunir232@gmail.com>
2023-11-09 14:23:09 +0500

    Basics of useQuery

--- React in Depth/react-query/src/App.jsx
+++ React in Depth/react-query/src/App.jsx
@@ -1,9 +1,37 @@
+import { useQuery, useMutation } from "react-query";
+
+const POSTS = [
+  { id: 1, title: "First Post" },
+  { id: 2, title: "Second Post" },
+];
+
 function App() {
+  const postQuery = useQuery({
+    // Unique identifer for your query, always takes an array
+    queryKey: ["posts"],
+    // Always Accepts a promise because it is async function
+    queryFn: () => wait(1000).then(() => [...POSTS]),
+  });
+
+  if (postQuery.isLoading) {
+    return <h1>Loading...</h1>;
+  }
+
+  if (postQuery.isError) {
+    return <pre>{JSON.stringify(postQuery.error)}</pre>;
+  }
+
   return (
     <div>
-      <h1>App</h1>
+      {postQuery.data.map((post) => (
+        <div key={post.id}>{post.title}</div>
+      ))}
     </div>
   );
+}
+
+function wait(duration) {
+  return new Promise((resolve) => setTimeout(resolve, duration));
 }
 
 export default App;
