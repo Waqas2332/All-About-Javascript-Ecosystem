d3159e27f0d0b7819eee14c44a7e08cc83483267
Waqas <wmunir232@gmail.com>
2023-11-09 14:08:30 +0500

    Fix small bug

--- React in Depth/react-query/src/main.jsx
+++ React in Depth/react-query/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { QueryClient, QueryClientProvider } from "react-query";
 
-const client = QueryClient();
+const client = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
