035018839eae413f444c7bc61304119f01a2736c
Waqas <wmunir232@gmail.com>
2023-11-23 15:11:47 +0500

    Done Basic Implementation of actions

---
+++ React in Depth/Redux Toolkit/redux-toolkit/src/features/counter/counterSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  count: 0,
+};
+
+export const counterSlice = createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    // Reducers contains all actions which are at the end anonymous functions
+    increment: (state) => {
+      state.count += 1;
+    },
+    decrement: (state) => {
+      state.count -= 1;
+    },
+  },
+});
+
+export const { increment, decrement } = counterSlice.actions;
+export default counterSlice.reducer;
