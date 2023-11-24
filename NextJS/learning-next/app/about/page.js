e40b225235710625a2e8eb3c21544d8aebe50391
Waqas <wmunir232@gmail.com>
2023-11-24 10:21:01 +0500

    Added Links

--- NextJS/learning-next/app/about/page.js
+++ NextJS/learning-next/app/about/page.js
@@ -1,7 +1,10 @@
+import Link from "next/link";
+
 export default function About() {
   return (
     <div>
       <h2>About Page</h2>
+      <Link href="/">Go To Homepage</Link>
     </div>
   );
 }
