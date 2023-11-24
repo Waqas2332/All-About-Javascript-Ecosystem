305396fc86b5494617d64dab9f790bf5f6f62ebf
Waqas <wmunir232@gmail.com>
2023-11-24 12:21:50 +0500

    Construct Path Programmatically

--- NodeJS/node-file-system/server.js
+++ NodeJS/node-file-system/server.js
@@ -1,8 +1,9 @@
 const fs = require("fs");
+const path = require("path");
 
 // Reading Files
 // In the callback function, data is in the form of buffer
-fs.readFile("./starter.txt", (err, data) => {
+fs.readFile(path.join(__dirname, "starter.txt"), (err, data) => {
   if (err) throw err;
   // converting buffer in string
   console.log(data.toString());
