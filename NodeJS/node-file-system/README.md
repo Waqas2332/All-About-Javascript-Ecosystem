35194c62fc9561d78bda06f447843d7c79c69b80
Waqas <wmunir232@gmail.com>
2023-11-24 12:37:28 +0500

    Managed All Functionalities in async fashion

--- NodeJS/node-file-system/README.md
+++ NodeJS/node-file-system/README.md
@@ -3,3 +3,5 @@
 Due to async nature of nodejs, tasks compelete in random order, for controlling that random order tasks in a specific way, we have two method :
 
 - Making CallBacks in Callbacks ( In this case, each callback will run after parent callback and flow of program will be in controlled )
+
+* Using async await ( fs.promises )
--- NodeJS/node-file-system/server.js
+++ NodeJS/node-file-system/server.js
@@ -1,23 +1,52 @@
-const fs = require("fs");
+const fsPromises = require("fs").promises;
 const path = require("path");
+
+async function fileOps() {
+  try {
+    const data = await fsPromises.readFile(
+      path.join(__dirname, "starter.txt"),
+      "utf-8"
+    );
+    await fsPromises.writeFile(path.join(__dirname, "promiseWrite.txt"), data);
+    await fsPromises.appendFile(
+      path.join(__dirname, "promiseWrite.txt"),
+      "\n\nNice To Meet You"
+    );
+    await fsPromises.rename(
+      // Older Name
+      path.join(__dirname, "promiseWrite.txt"),
+      // New Name
+      path.join(__dirname, "promiseComplete.txt")
+    );
+    const newData = await fsPromises.readFile(
+      path.join(__dirname, "promiseComplete.txt"),
+      "utf-8"
+    );
+    console.log(newData);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+fileOps();
 
 // Reading Files
 // In the callback function, data is in the form of buffer
-fs.readFile(path.join(__dirname, "starter.txt"), (err, data) => {
-  if (err) throw err;
-  // converting buffer in string
-  console.log(data.toString());
-});
+// fs.readFile(path.join(__dirname, "starter.txt"), (err, data) => {
+//   if (err) throw err;
+//   // converting buffer in string
+//   console.log(data.toString());
+// });
 
 // writeFile() ==> takes path to the file , content which should be written, and then returns a callback
-fs.writeFile(path.join(__dirname, "reply.txt"), "Nice to meet you", (err) => {
-  if (err) throw err;
-  // converting buffer in string
-  console.log("Write Operation Completed Successfully");
-});
+// fs.writeFile(path.join(__dirname, "reply.txt"), "Nice to meet you", (err) => {
+//   if (err) throw err;
+//   // converting buffer in string
+//   console.log("Write Operation Completed Successfully");
+// });
 
-fs.appendFile(path.join(__dirname, "test.txt"), "\nChecking update", (err) => {
-  if (err) throw err;
-  // converting buffer in string
-  console.log("Append Operation Completed Successfully");
-});
+// fs.appendFile(path.join(__dirname, "test.txt"), "\nChecking update", (err) => {
+//   if (err) throw err;
+//   // converting buffer in string
+//   console.log("Append Operation Completed Successfully");
+// });
