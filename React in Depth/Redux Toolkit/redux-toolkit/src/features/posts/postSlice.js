eed3d81c3f22775a8556337d9484bc74613244f0
Waqas <wmunir232@gmail.com>
2023-11-25 13:01:59 +0500

    Refactored Project for Async Thunk

--- React in Depth/Redux Toolkit/redux-toolkit/src/features/posts/postSlice.js
+++ React in Depth/Redux Toolkit/redux-toolkit/src/features/posts/postSlice.js
@@ -1,33 +1,24 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { sub } from "date-fns";
 
-const initialState = [
-  {
-    id: "1",
-    title: "Learning Redux Toolkit",
-    content: "Hey, I am learning it",
-    date: sub(new Date(), { minutes: 10 }).toISOString(),
-  },
-  {
-    id: "2",
-    title: "All About Slices",
-    content: "Slice things down to solve them quickly",
-    date: sub(new Date(), { minutes: 5 }).toISOString(),
-  },
-];
+const initialState = {
+  posts: [],
+  status: "idle", // "idle","loading","succeeded","failed",
+  error: null,
+};
 
 const postSlice = createSlice({
   name: "posts",
   initialState,
   reducers: {
     postAdded: (state, action) => {
-      state.push(action.payload);
+      state.posts.push(action.payload);
     },
   },
 });
 
 export default postSlice.reducer;
 
-export const selectAllPosts = (state) => state.posts;
+export const selectAllPosts = (state) => state.posts.posts;
 
 export const { postAdded } = postSlice.actions;
