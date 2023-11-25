53463629a94cea6365647238bf606e1f1ba716bf
Waqas <wmunir232@gmail.com>
2023-11-25 13:14:42 +0500

    Created Fetch Async Thunk

--- React in Depth/Redux Toolkit/redux-toolkit/src/features/posts/postSlice.js
+++ React in Depth/Redux Toolkit/redux-toolkit/src/features/posts/postSlice.js
@@ -1,11 +1,23 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
 import { sub } from "date-fns";
+
+const POST_URL = "https://jsonplaceholder.typicode.com/posts";
 
 const initialState = {
   posts: [],
   status: "idle", // "idle","loading","succeeded","failed",
   error: null,
 };
+
+export const fetchPosts = createAsyncThunk("posts/fetchPosts", async () => {
+  try {
+    const response = await axios.get(POST_URL);
+    return [...response.data];
+  } catch (error) {
+    return error.message;
+  }
+});
 
 const postSlice = createSlice({
   name: "posts",
@@ -14,6 +26,25 @@ const postSlice = createSlice({
     postAdded: (state, action) => {
       state.posts.push(action.payload);
     },
+  },
+  extraReducers(builder) {
+    builder
+      .addCase(fetchPosts.pending, (state, action) => {
+        state.status = "loading";
+      })
+      .addCase(fetchPosts.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        let min = 1;
+        const loadedPosts = action.payload.map((post) => {
+          post.date = sub(new Date(), { minutes: min++ }).toISOString();
+          return post;
+        });
+        state.posts = state.posts.concat(loadedPosts);
+      })
+      .addCase(fetchPosts.rejected, (state, action) => {
+        state.status = "error";
+        state.error = action.error.message;
+      });
   },
 });
 
