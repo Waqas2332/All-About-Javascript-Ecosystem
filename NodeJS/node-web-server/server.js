07df27410ab289a5e27bf17f2b7a506398851dbe
Waqas <wmunir232@gmail.com>
2023-12-02 20:17:14 +0500

    Created Minimal Server

--- NodeJS/node-web-server/server.js
+++ NodeJS/node-web-server/server.js
@@ -2,5 +2,21 @@ const http = require("http");
 const path = require("path");
 const fs = require("fs");
 const fsPromises = require("fs").promises;
-
+const EventEmitter = require("events");
 const logEvents = require("./logEvents");
+
+class Emitter extends EventEmitter {}
+
+// Initialize Object
+const myEmitter = new Emitter();
+
+// Creating basic server
+const PORT = process.env.PORT || 8000;
+
+const server = http.createServer((req, res) => {
+  console.log(req.url, req.method);
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is running on PORT ${PORT}`);
+});
