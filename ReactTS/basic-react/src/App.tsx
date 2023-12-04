1be989993de37b3d715d034eb435d00de2101d5c
Waqas <wmunir232@gmail.com>
2023-12-04 17:05:03 +0500

    Added typed useState

--- ReactTS/basic-react/src/App.tsx
+++ ReactTS/basic-react/src/App.tsx
@@ -1,13 +1,26 @@
+import { useState } from "react";
+
 import CourseGoal from "./components/CourseGoal.tsx";
 import Header from "./components/Header.tsx";
 import goalsImg from "./assets/goals.jpg";
 
+type CourseGoal = {
+  title: string;
+  description: string;
+  id: number;
+};
+
 export default function App() {
+  const [goals, setGoals] = useState<CourseGoal[]>([]);
+
+  function handleAddGoal() {}
+
   return (
     <main>
       <Header image={{ src: goalsImg, alt: "A List of Goals" }}>
         <h1>Your Course Goals</h1>
       </Header>
+      <button>Add Goal</button>
       <CourseGoal
         title="Learn React + TS"
         description="Learn it from the ground up"
