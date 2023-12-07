5a1910ed50a41825d19fc824bc1357d8a0f1c112
Waqas <wmunir232@gmail.com>
2023-12-07 14:59:08 +0500

    Added the side drawer

--- MERN Stack/share-places/client/src/shared/components/navigation/MainNavigation.js
+++ MERN Stack/share-places/client/src/shared/components/navigation/MainNavigation.js
@@ -2,22 +2,42 @@ import MainHeader from "./MainHeader";
 import "./MainNavigation.css";
 import { Link } from "react-router-dom";
 import NavLinks from "./NavLink";
+import SideDrawer from "./SideDrawer";
+import { useState } from "react";
+import Backdrop from "../UI/Backdrop";
 
 function MainNavigation(props) {
+  const [isOpen, setIsOpen] = useState(false);
+  function closeDrawer() {
+    setIsOpen(false);
+  }
   return (
-    <MainHeader>
-      <button className="main-navigation__menu-btn">
-        <span />
-        <span />
-        <span />
-      </button>
-      <h1 className="main-navigation__title">
-        <Link to="/">YourPlaces</Link>
-      </h1>
-      <nav>
-        <NavLinks />
-      </nav>
-    </MainHeader>
+    <>
+      {isOpen && (
+        <SideDrawer>
+          <nav className="main-navigation__drawer-nav">
+            <NavLinks />
+          </nav>
+        </SideDrawer>
+      )}
+      {isOpen && <Backdrop onClick={closeDrawer} />}
+      <MainHeader>
+        <button
+          className="main-navigation__menu-btn"
+          onClick={() => setIsOpen(true)}
+        >
+          <span />
+          <span />
+          <span />
+        </button>
+        <h1 className="main-navigation__title">
+          <Link to="/">YourPlaces</Link>
+        </h1>
+        <nav className="main-navigation__header-nav">
+          <NavLinks />
+        </nav>
+      </MainHeader>
+    </>
   );
 }
 export default MainNavigation;
---
+++ MERN Stack/share-places/client/src/shared/components/navigation/SideDrawer.js
@@ -0,0 +1,7 @@
+import "./SideDrawer.css";
+import ReactDOM from "react-dom";
+
+export default function SideDrawer(props) {
+  const content = <aside className="side-drawer">{props.children}</aside>;
+  return ReactDOM.createPortal(content, document.getElementById("drawer-hook"));
+}
