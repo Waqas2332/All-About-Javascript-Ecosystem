e7e5a2ba2b6a43db442e0a2b33c67ffd82cd6897
Waqas <wmunir232@gmail.com>
2023-12-07 12:21:27 +0500

    Handled form submit event

--- ReactTS/basic-react/src/App.tsx
+++ ReactTS/basic-react/src/App.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import Header from "./components/Header.tsx";
 import goalsImg from "./assets/goals.jpg";
 import CourseGoalList from "./components/CourseGoalList.tsx";
+import NewGoal from "./components/NewGoal.tsx";
 
 export type CourseGoal = {
   title: string;
@@ -35,7 +36,7 @@ export default function App() {
       <Header image={{ src: goalsImg, alt: "A List of Goals" }}>
         <h1>Your Course Goals</h1>
       </Header>
-      <button onClick={handleAddGoal}>Add Goal</button>
+      <NewGoal />
       <CourseGoalList goals={goals} onDeleteGoal={handleDeleteGoal} />
     </main>
   );
