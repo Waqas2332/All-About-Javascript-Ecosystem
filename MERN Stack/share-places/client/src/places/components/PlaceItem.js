ee8f5c5bd63228c876dcfe52ba1dd7073b61e367
Waqas <wmunir232@gmail.com>
2023-12-08 10:30:33 +0500

    Made Modal component for displaying map

--- MERN Stack/share-places/client/src/places/components/PlaceItem.js
+++ MERN Stack/share-places/client/src/places/components/PlaceItem.js
@@ -1,25 +1,52 @@
 import Card from "../../shared/components/UI/Card";
 import "./PlaceItem.css";
 import Button from "../../shared/components/FormElements/Button";
+import { useState } from "react";
+import Modal from "../../shared/components/UI/Modal";
 
 export default function PlaceItem(props) {
+  const [showMap, setShowMap] = useState(false);
+
+  function openModalHandler() {
+    setShowMap(true);
+  }
+
+  function closeModalHandler() {
+    setShowMap(false);
+  }
   return (
-    <li className="place-item">
-      <Card className="place-item__content">
-        <div className="place-item__image">
-          <img src={props.image} alt={props.title} />
+    <>
+      <Modal
+        show={showMap}
+        onCancel={closeModalHandler}
+        header={props.address}
+        contentClass="place-item__modal-content"
+        footerClass="place-item__modal-actions"
+        footer={<Button onClick={closeModalHandler}>Close</Button>}
+      >
+        <div className="map-container">
+          <h2>THE MAP</h2>
         </div>
-        <div className="place-item__info">
-          <h2>{props.title}</h2>
-          <h3>{props.address}</h3>
-          <p>{props.description}</p>
-        </div>
-        <div className="place-item__actions">
-          <Button inverse>VIEW ON MAP</Button>
-          <Button to={`/places/${props.id}`}>EDIT</Button>
-          <Button danger>DELETE</Button>
-        </div>
-      </Card>
-    </li>
+      </Modal>
+      <li className="place-item">
+        <Card className="place-item__content">
+          <div className="place-item__image">
+            <img src={props.image} alt={props.title} />
+          </div>
+          <div className="place-item__info">
+            <h2>{props.title}</h2>
+            <h3>{props.address}</h3>
+            <p>{props.description}</p>
+          </div>
+          <div className="place-item__actions">
+            <Button inverse onClick={openModalHandler}>
+              VIEW ON MAP
+            </Button>
+            <Button to={`/places/${props.id}`}>EDIT</Button>
+            <Button danger>DELETE</Button>
+          </div>
+        </Card>
+      </li>
+    </>
   );
 }
