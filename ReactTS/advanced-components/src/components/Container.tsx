25100a558873be0468c050e11c3ebd6c11e659a0
waqas <wmunir232@gmail.com>
2024-01-10 13:25:52 +0500

    Done with it

--- ReactTS/advanced-components/src/components/Container.tsx
+++ ReactTS/advanced-components/src/components/Container.tsx
@@ -1,13 +1,14 @@
-import { ElementType } from "react";
+import { ElementType, ReactNode } from "react";
 
 type ContainerProps = {
   as: ElementType;
+  children: ReactNode;
 };
 
-function Container({ as }: ContainerProps) {
+function Container({ as, children }: ContainerProps) {
   const Component = as;
 
-  return <Component />;
+  return <Component>{children}</Component>;
 }
 
 export default Container;
